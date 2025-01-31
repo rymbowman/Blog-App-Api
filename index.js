caefc707ddd3554c6f57a1af32f1ad1686d9c746
Ryan Bowman <rymbowman@gmail.com>
2025-01-31 18:28:52 -0500

    ..

--- index.js
+++ index.js
@@ -29,18 +29,6 @@ app.use("/api/posts", postsRouter);
 app.use("/api/auth", authRouter);
 app.use("/api", commentsRouter);
 
-// Serve static files from the frontend dist folder
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const staticMiddleware = express.static(
-  path.join(__dirname, "../Blog-App-Client/dist")
-);
-app.use(staticMiddleware);
-
-// Serve React frontend for all other requests
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Blog-App-Client/dist", "index.html"));
-});
-
 // server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
